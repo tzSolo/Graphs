835dcb9c527fa15fe1d323d0ed5731299d95093e
tzSolo <t0504114734@gmail.com>
2025-08-17 18:29:58 +0300

    Cleaner code for select category popup.

--- scripts/injectCategory.js
+++ scripts/injectCategory.js
@@ -1,41 +1,77 @@
-window.onload = () => {
+if (!window.isPopupDisplayed) {
+    window.isPopupDisplayed = true;
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", init);
+    } else {
+        init();
+    }
+}
+
+function init() {
+    const fontLinkHtml = document.createElement("link");
+    fontLinkHtml.rel = "stylesheet";
+    fontLinkHtml.href = "https://fonts.googleapis.com/css2?family=Noto+Sans+Hebrew&family=Roboto&display=swap";
+    document.head.appendChild(fontLinkHtml);
+
     const overlayDiv = document.createElement("div");
     overlayDiv.id = "overlay";
 
     const calculatorDiv = document.createElement("div");
     calculatorDiv.id = "select-category";
 
-    calculatorDiv.innerHTML = `
-    <h2>לאיזו קטגוריה משתייך האתר ?</h2>
-        <form>
-            <div>
-                <input type="radio" name="category" id="work" value="Work">
-                <label for="work">עבודה</label>
-            </div>
-            <div>
-                <input type="radio" name="category" id="studies" value="Studies">
-                <label for="studies">לימודים</label>
-            </div>
-            <div>
-                <input type="radio" name="category" id="shopping" value="Shopping">
-                <label for="shopping">קניות</label>
-            </div>
-            <div>
-                <input type="radio" name="category" id="news" value="News">
-                <label for="news">חדשות</label>
-            </div>
-            <div>
-                <input type="radio" name="category" id="another" value="Another">
-                <label for="another">אחר</label>
-            </div>
+    const categoryForm = document.createElement("form");
+    categoryForm.innerHTML = '<h2>לאיזו קטגוריה משתייך האתר ?</h2>';
+
+    const arrCategoryOptions = [
+        {
+            label: "work",
+            hebrew: "עבודה"
+        },
+        {
+            label: "studies",
+            hebrew: "לימודים",
+        },
+        {
+            label: "shopping",
+            hebrew: "קניות"
+        },
+        {
+            label: "news",
+            hebrew: "חדשות",
+        },
+        {
+            label: "another",
+            hebrew: "אחר",
+        }
+    ];
+
+    const optionHtml = `       
+            <input type="radio" name="category" required>
+            <label></label>`;
+
+    arrCategoryOptions.forEach((option) => {
+        const containerDiv = document.createElement("div");
+        containerDiv.innerHTML = optionHtml;
+        const input = containerDiv.querySelector("input");
+        input.id = option.label;
+        input.value = option.label.toUpperCase();
+        const label = containerDiv.querySelector("label");
+        label.htmlFor = option.label;
+        label.innerText = option.hebrew;
+        categoryForm.append(containerDiv);
+    });
+
+    const submissionHtml = `
             <div id="cont-another-category">
                 <label for="another-category">קטגוריה חדשה :</label>
                 <input type="text" name="another-category" id="another-category">
             </div>
-            <button type="button">אישור</button>
-        </form>
-    </div>
-`
+            <button type="button">אישור</button>`;
+    categoryForm.innerHTML += submissionHtml;
+
+    calculatorDiv.append(categoryForm);
+
     document.body.append(calculatorDiv, overlayDiv);
 
     const another = calculatorDiv.querySelector("#another");
@@ -60,6 +96,26 @@ window.onload = () => {
 
     const confirmCategoryButton = selectCategoryForm.querySelector("button");
     confirmCategoryButton.addEventListener('click', () => {
+        let selectedCategory = null;
+
+        const checkedRadio = calculatorDiv.querySelector("input[name='category']:checked");
+        if (checkedRadio) {
+            if (checkedRadio.id === "another") {
+                const anotherInput = calculatorDiv.querySelector("#another-category");
+                selectedCategory = anotherInput.value.trim();
+                if (!selectedCategory) {
+                    alert("לא נבחרה קטגוריה !");
+                    return;
+                }
+            } else {
+                selectedCategory = checkedRadio.value;
+            }
+        }
+        else {
+            alert("לא נבחרה קטגוריה !");
+            return;
+        }
+
         [calculatorDiv, overlayDiv].forEach(div => div.remove());
     });
 }
